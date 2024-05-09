e65cc49b809476bffc415cbc6913d6ee8f210441
Bishesh Marasini <marasinibishesh@gmail.com>
2024-05-09 12:58:00 +0545

    01_variable completed

--- 01_variables.js
+++ 01_variables.js
@@ -1,2 +1,17 @@
 const accountId=442355
-let 
+let accountEmail="bishesh@gmail.com"
+var accountPassword="12345"
+accountCity="Butwal"
+//accountId=2 //not allowded
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+accountEmail="Bishesh@"
+accountPassword="12"
+accountCity="Kathmandu"//can be declared but not prefered
+let accountState; //if value not given then shows undefined
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//{} ->scope
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/

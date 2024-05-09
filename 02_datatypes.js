ac1c0526097e2825d9d91d26919833b0fb4d4c2b
Bishesh Marasini <marasinibishesh@gmail.com>
2024-05-09 13:33:46 +0545

    Datatype

---
+++ 02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version
+//alert(3 + 3)//we are using nodejs,not browser // this alert() syntax is older approach
+console.log(3 +
+     3)//avoid writing like this for readability
+
+console.log("Bishesh")
+let name="Bishesh"//string
+let age=20//number
+let isLoggedIn=false//bollean
+// number => 2 to power 53 range
+//bigint
+//string=>""
+//boolean=> true/false
+//null =>standalone value
+//undefined => value not assigned yet
+//symbol =>unique
+
+//object
+console.log(typeof undefined)//undefined
+console.log(typeof null);//object

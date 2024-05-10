0de3a344ecc41e25dba9d923fdd55e509c45df17
Bishesh Marasini <marasinibishesh@gmail.com>
2024-05-10 16:20:40 +0545

    Comparison2

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -15,6 +15,7 @@ console.log(id==anotherId);//false
 console.log(id===anotherId);//false
 //for declaring BigInt
 const bigNumber=1234567890123456789n//use n for bigint
+console.log(typeof bigNumber);
 //for declaring objects
 const heros=["Shaktiman","Naagraj","Bishesh"]
 let myObj={
@@ -25,7 +26,9 @@ let myObj={
 const myfunction=function(){
     console.log("Hello World");
 }
-console.log(typeof outsideTemp);
+console.log(typeof heros);
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
 
 
 
@@ -33,3 +36,24 @@ console.log(typeof outsideTemp);
 
 //Non-Primitive Datatype
 /* 4 types :Object,Array,Function,Date */
+//********************************************************************** */
+//Stack(Primitive), Heap(Non-Primitive)
+/*when we use stack memory then we get a copy of the variable and
+ when we use heap memory then we get reference of the original variable */
+ //For primitive example
+ let myYoutubeName="BisheshMarasini"
+ let anothername=myYoutubeName
+ anothername="oppo"
+ //console.log(myYoutubeName);
+ //console.log(anothername);
+ //For reference example
+ let userOne={
+    email:"user@gmail.com",
+    name:"Bishesh",
+ }
+ let userTwo=userOne
+ userTwo.email="stack@gmail.com"
+ userTwo.name="oppo"
+ console.log(userOne);
+ console.log(userTwo);
+
